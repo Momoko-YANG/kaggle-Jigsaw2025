07eb62bd27af35187b4a163c645b9fd35dca874d
Momoko-YANG <yangmy1215@gmail.com>
2025-10-19 17:32:28 +0900

    add init file

---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+"""
+Rule Violation Detection Package
+A machine learning system for detecting rule violations in text.
+"""
+
+version = "0.1.0"
+author = "Momoko Yang"
+email = "yangmy1215@gmail.com"
+
+# Package-level imports for convenience
+from src.data.loader import DataLoader
+from src.data.preprocessor import TextPreprocessor
+from src.models.embedding_model import EmbeddingModel
+from src.models.trainer import ModelTrainer
+from src.features.centroids import CentroidBuilder
+from src.inference.predictor import ViolationPredictor
+
+__all__ = [
+    "DataLoader",
+    "TextPreprocessor",
+    "EmbeddingModel",
+    "ModelTrainer",
+    "CentroidBuilder",
+    "ViolationPredictor",
+]
